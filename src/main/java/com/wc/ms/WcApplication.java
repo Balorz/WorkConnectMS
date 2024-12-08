8c5ce6d4bdfa70972e4b07476a3675409536d03e
Balorz <aryandubey1611@gmail.com>
2024-12-08 18:38:25 +0530

    First Commit For MS

---
+++ src/main/java/com/wc/ms/WcApplication.java
@@ -0,0 +1,13 @@
+package com.wc.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcApplication.class, args);
+	}
+
+}
